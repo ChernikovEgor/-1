f88b4e741405ee28edba8c17efcfec29e649fb4d
ChernikovEgor <74093325+ChernikovEgor@users.noreply.github.com>
2020-11-12 22:29:06 +0300

    Add files via upload

---
+++ 123.py
@@ -0,0 +1,21 @@
+ewe = "Privet"
+myName = "Egor"
+myAge = "17"
+mySurname = "Chernikov"
+myTown = "Novgorod"
+myStreet = "Rozvazha"
+mySchool = "NSK"
+nOn = "ya nichem ne zanimaus"
+space = "kosmos"
+Gg = "major po csgo"
+monEy = "million"
+puTin = "Putin"
+wOt = "world of tanks"
+doTa = "nenavizhy dota2"
+driNk = "zeleniy chai lipton"
+wew = "Poka"
+print("Vsem",ewe,"menya zovyt",myName,"mne",myAge,"moya familiya",mySurname,"ya zhivy v",myTown,"na ylice",myStreet)
+print("I ychys v",mySchool,"po zhizne",nOn,"no zato ya letal v",space,"a eshe viglar",Gg,"za chto ya polychil",monEy,"tisyach dolarov")
+print("Chyt ne zabil skazat chto ya znakom s",puTin,)
+print("V svobodnoe vremya ya lublu igrat v kompyter i moya lubimay igra eto",wOt,"i v otlichii ot my drug Dima ya",doTa)
+print("ya ochen lublu pit",driNk,"i na etom vse",wew)
